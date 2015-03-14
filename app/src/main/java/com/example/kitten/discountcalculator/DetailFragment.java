5d99ba5cc3e5f43e196e0dfe777d1e14ef78c667
Nicole <nfelch121394@gmail.com>
2015-03-14 13:03:46 -0400

    basic functionality and layouts

--- app/src/main/java/com/example/kitten/discountcalculator/DetailFragment.java
+++ app/src/main/java/com/example/kitten/discountcalculator/DetailFragment.java
@@ -1,21 +1,34 @@
 package com.example.kitten.discountcalculator;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by kitten on 3/14/15.
  */
 public class DetailFragment extends Fragment {
+    TextView percentageTextView;
+    Integer mPercentage;
+
     public DetailFragment() {
 
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_detail, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+        percentageTextView = (TextView) rootView.findViewById(R.id.selected_discount);
+
+        Intent intent = getActivity().getIntent();
+        if(intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
+            mPercentage = (Integer) intent.getIntExtra(Intent.EXTRA_TEXT, 0);
+            percentageTextView.setText(Integer.toString(mPercentage));
+        }
+        return rootView;
     }
 }
--- app/src/main/java/com/example/kitten/discountcalculator/DiscountsFragment.java
+++ app/src/main/java/com/example/kitten/discountcalculator/DiscountsFragment.java
@@ -1,15 +1,30 @@
 package com.example.kitten.discountcalculator;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-/**
- * Created by kitten on 3/14/15.
- */
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
 public class DiscountsFragment extends Fragment {
+
+    private ListView listView;
+    private Integer[] percentages = {
+        30,
+        40,
+        50,
+        70
+    };
+    private ArrayAdapter mDiscountAdapter;
 
     public DiscountsFragment(){
 
@@ -17,6 +32,29 @@ public class DiscountsFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_discounts, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_discounts, container, false);
+        listView = (ListView) rootView.findViewById(R.id.listView_discounts);
+        List<Integer> percentageList = new ArrayList<Integer>(Arrays.asList(percentages));
+        mDiscountAdapter = new ArrayAdapter(
+                getActivity(),
+                R.layout.list_item_discount,
+                R.id.discount_percentage,
+                percentageList
+        );
+        listView.setAdapter(mDiscountAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Integer percentage = (Integer) mDiscountAdapter.getItem(i);
+                Intent intent = new Intent(getActivity(), DetailActivity.class);
+                intent.putExtra(Intent.EXTRA_TEXT, percentage);
+                startActivity(intent);
+            }
+
+        });
+        return rootView;
     }
+
 }
