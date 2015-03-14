1f6f04b49292050c762a10b2281226333cddb84a
Nicole <nfelch121394@gmail.com>
2015-03-14 10:07:20 -0400

    Initial commit

---
+++ app/src/main/java/com/example/kitten/discountcalculator/DetailFragment.java
@@ -0,0 +1,21 @@
+package com.example.kitten.discountcalculator;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by kitten on 3/14/15.
+ */
+public class DetailFragment extends Fragment {
+    public DetailFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_detail, container, false);
+    }
+}
