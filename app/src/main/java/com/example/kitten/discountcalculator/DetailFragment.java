0501e6a42e4028f46b8537ed8764ed0923f5d84f
Nicole <nfelch121394@gmail.com>
2015-03-19 13:07:13 -0400

    layout improvements, number formatting

--- app/src/main/java/com/example/kitten/discountcalculator/DetailFragment.java
+++ app/src/main/java/com/example/kitten/discountcalculator/DetailFragment.java
@@ -11,14 +11,16 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.text.DecimalFormat;
+
 /**
  * Created by kitten on 3/14/15.
  */
 public class DetailFragment extends Fragment {
     TextView percentageTextView;
     Integer mPercentage;
-    Double mOriginalPrice;
-    Double mNewPrice;
+    Integer mOriginalPrice;
+    Integer mNewPrice;
     TextView finalPriceTextView;
     EditText editText;
 
@@ -40,23 +42,20 @@ public class DetailFragment extends Fragment {
             editText.addTextChangedListener(new TextWatcher() {
                 @Override
                 public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) {
-
                 }
 
                 @Override
                 public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
                     if(!(editText.getText().toString().equals(""))) {
-                        mOriginalPrice = Double.valueOf(editText.getText().toString());
                         calculatePrice();
                     }
                     else {
-                        finalPriceTextView.setText(Double.toString(0.0));
+                        finalPriceTextView.setText(integerToCurrency(0));
                     }
                 }
 
                 @Override
                 public void afterTextChanged(Editable editable) {
-                    finalPriceTextView.setText("$" + finalPriceTextView.getText().toString());
                 }
             });
         }
@@ -64,8 +63,24 @@ public class DetailFragment extends Fragment {
     }
 
     public void calculatePrice() {
-        Double discount = mOriginalPrice * (Double.valueOf(mPercentage) / 100.00);
+        mOriginalPrice = currencyToInteger(editText.getText().toString());
+        Double percentOff = Double.valueOf(mPercentage) / 100.0;
+        Double discountDouble = Double.valueOf(mOriginalPrice) * percentOff;
+        Integer discount = (discountDouble).intValue();
         mNewPrice = mOriginalPrice - discount;
-        finalPriceTextView.setText(Double.toString(mNewPrice));
+        finalPriceTextView.setText(integerToCurrency(mNewPrice));
+    }
+
+    public Integer currencyToInteger(String currency) {
+        Double d = Double.valueOf(currency);
+        Double c = d * 100;
+        int cents = (c).intValue();
+        return cents;
+    }
+
+    public String integerToCurrency(Integer cents) {
+        Double d = cents.doubleValue() / 100.0;
+        DecimalFormat df = new DecimalFormat("$0.00");
+        return df.format(d);
     }
 }
